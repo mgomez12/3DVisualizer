a173e24eb5a8c1aa7f02af2ea9f4db56088c9597
Miguel Gomez <mgomez12@mit.edu>
2020-11-18 01:52:30 -0500

    rotation module minus pulse input

--- 3DVisualizer.srcs/sources_1/migo/new/angle_gen.sv
+++ 3DVisualizer.srcs/sources_1/migo/new/angle_gen.sv
@@ -24,10 +24,47 @@ module angle_gen(
     input vsync,
     input clk,
     input rst,
-    output signed [11:0]sin_out, cos_out,
-    output valid_angle
+    output [11:0] angles,
+    output signed [11:0] sin_out[3], cos_out[3]
     );
-    assign sin_out = '0;
-    assign cos_out = 12'b011111111111;
-    assign valid_angle = vsync;
+    logic [11:0] out [3];
+    logic signed [9:0] rate [3] = {10'd1, -10'd1, 10'd1};
+    logic [11:0] final_in [3];
+    logic signed [9:0] last[3];
+    logic signed [9:0] in [3];
+    logic [28:0] cordic_out[3];
+    logic out_valid[3];
+    assign angles = in[0];
+    cordic_0 u_cordic0 (.aclk(clk), .s_axis_phase_tvalid(vsync), 
+                        .s_axis_phase_tdata(final_in[0]), .m_axis_dout_tvalid(out_valid[0]), 
+                        .m_axis_dout_tdata(cordic_out[0]));
+    cordic_0 u_cordic1 (.aclk(clk), .s_axis_phase_tvalid(vsync), 
+                        .s_axis_phase_tdata(final_in[1]), .m_axis_dout_tvalid(read), 
+                        .m_axis_dout_tdata(cordic_out[1]));
+    cordic_0 u_cordic2 (.aclk(clk), .s_axis_phase_tvalid(vsync), 
+                        .s_axis_phase_tdata(final_in[2]), .m_axis_dout_tvalid(out_valid[2]), 
+                        .m_axis_dout_tdata(cordic_out[2]));
+    
+    assign cos_out[0] = cordic_out[0][11:0];
+    assign sin_out[0] = cordic_out[0][27:16];
+    assign cos_out[1] = cordic_out[1][11:0];
+    assign sin_out[1] = cordic_out[1][27:16];
+    assign cos_out[2] = cordic_out[2][11:0];
+    assign sin_out[2] = cordic_out[2][27:16];
+    always_comb begin
+        for (int i = 0; i < 3; i++) begin
+            in[i] = last[i] + rate[i];
+            final_in[i] = {in[i][9], in[i][9], in[i]};
+        end
+    end
+  
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            last <= '{default:0};
+        end else begin
+            if (vsync) begin 
+                for (int i = 0; i < 3; i++) last[i] <= in[i];
+            end
+        end
+    end
 endmodule
--- 3DVisualizer.srcs/sources_1/migo/new/graphics_top.sv
+++ 3DVisualizer.srcs/sources_1/migo/new/graphics_top.sv
@@ -54,47 +54,59 @@ module graphics_top(
     logic [11:0] addr_wr;
     logic [11:0] addr_rd;
     logic write;
-    logic write_en[2];
+    logic [1:0] write_en;
     logic delete;
     logic delete_en[2];
     logic [11:0] addr_delete;
     logic [11:0] pixel_out[2];
     
     logic [8:0] vcount_del[2];
+    logic [9:0] hcount_del[2];
     logic hsync_del[2];
     logic vsync_del[2];
+    logic [11:0] pixel;
+    logic [8:0] last_row;
+    logic begin_calculating;
+    logic [11:0] test_point;
+    logic signed [11:0] sin[3], cos[3];
     
+    ila_0 ila(.clk(clk_25mhz), .probe0(hcount_del[1]), .probe1(vcount_del[1]), .probe2(hsync_del[1]), .probe3(vsync_del[1]), .probe5(addr_delete), .probe4(pixel));
+    ila_1 ila2(.clk(clk_100mhz), .probe0(sin[0]), .probe1(hcount_delay), .probe2(write_en), .probe3(begin_calculating), .probe4(point_coord[0]),
+               .probe5(point_coord[1]), .probe6(vcount_new), .probe7(valid_point), .probe8(vsync_converted), .probe9(test_point));
     clk_wiz_0 clk_25 (.clk_out1(clk_25mhz), .reset(btnu), .locked(locked), .clk_in1(clk_100mhz));
     xvga u_xvga(.vclock_in(clk_25mhz),.hcount_out(hcount), .vcount_out(vcount), .vsync_out(vsync), .hsync_out(hsync), .blank_out(blank));
-    transformation #(.POINT_WIDTH(12), .NUM_POINTS(20)) u_transformation (.clk(clk_100mhz), .valid(vsync_converted), .rst(btnu), .scale(1), .sin_in({12'b0, 12'b0, 12'b0}),
-                                                      .cos_in({12'b0, 12'b0, 12'b0}), .point_out(point_coord), .valid_out(valid_point));
-    blk_mem_gen_1 pixels0(.clka(clk_100mhz), .wea(write_en[0]), .addra(addr_wr), .dina(12'hfff), .clkb(clk_25mhz), 
+    transformation #(.POINT_WIDTH(12), .NUM_POINTS(20)) u_transformation (.clk(clk_100mhz), .valid(begin_calculating), .rst(btnu), .scale(12'b011111111111), .sin_in(sin),
+                   .cos_in(cos), .point_out(point_coord), .valid_out(valid_point));
+    blk_mem_gen_1 pixels0(.clka(clk_100mhz), .wea(write_en[0]), .addra(addr_wr), .dina(12'h0f0), .clkb(clk_25mhz), 
                           .web(delete_en[0]), .addrb(addr_delete), .dinb('0), .doutb(pixel_out[0]));
                           
-    blk_mem_gen_1 pixels1(.clka(clk_100mhz), .wea(write_en[1]), .addra(addr_wr), .dina(12'hfff), .clkb(clk_25mhz), 
+    blk_mem_gen_1 pixels1(.clka(clk_100mhz), .wea(write_en[1]), .addra(addr_wr), .dina(12'h0f0), .clkb(clk_25mhz), 
                           .web(delete_en[1]), .addrb(addr_delete), .dinb('0), .doutb(pixel_out[1]));
+    angle_gen u_angle(.vsync(vsync_converted), .clk(clk_100mhz), .rst(btnu), .sin_out(sin), .cos_out(cos));
 
 
     always_comb begin
-        write = valid_point && (point_coord[1][9:3] == (vcount_new[9:3] == 159 ? 0 : vcount_new[9:3] + 1));
+        write = valid_point && (point_coord[1][8:2] == (vcount_new[8:2] == 119 ? 0 : vcount_new[8:2] + 1));
         write_en[0] = write && vcount_new[2];
         write_en[1] = write && ~vcount_new[2];
         addr_wr = point_coord[1][1:0]*640+ point_coord[0];
         delete_en[0] = vcount[2] == 0 && (hcount < 640 && vcount <480);
         delete_en[1] = vcount[2] == 1 && (hcount < 640 && vcount <480);
         addr_delete = vcount[1:0]*640 + hcount;
-        
-        {vga_r, vga_g, vga_b} = (vcount_del[1][2] == 1'b1 ? pixel_out[1] : pixel_out[0]);
+        pixel = (vcount_del[1][2] == 1'b1 ? pixel_out[1] : pixel_out[0]);
+        {vga_r, vga_g, vga_b} = pixel;
         vga_hs = hsync_del[1];
         vga_vs = vsync_del[1];
+        begin_calculating = (last_row != vcount_new) && vcount_new[1:0] == 2'b00;
     end
     
     
     always_ff @(posedge clk_25mhz) begin
         if (btnu) begin
-            hsync_del = '{default:0};
-            vsync_del = '{default:0};
-            vcount_del = '{default:0};
+            hsync_del <= '{default:0};
+            vsync_del <= '{default:0};
+            vcount_del <= '{default:0};
+            hcount_del <= '{default:0};
         end else begin
             hsync_del[0] <= hsync;
             hsync_del[1] <= hsync_del[0];
@@ -102,6 +114,8 @@ module graphics_top(
             vsync_del[1] <= vsync_del[0];
             vcount_del[0] <= vcount;
             vcount_del[1] <= vcount_del[0];
+            hcount_del[0] <= hcount;
+            hcount_del[1] <= hcount_del[0];
         end
     end
     
@@ -117,7 +131,9 @@ module graphics_top(
             hsync_converted <= 0;
             hsync_old <= 0;
             vsync_old <= 0;
+            last_row <= 0;
         end else begin
+            last_row <= vcount_new;
             if (hsync_converted) vcount_new <= vcount_new + 1;
             if (vsync_converted) vcount_new <= 0;
         
@@ -141,7 +157,7 @@ module graphics_top(
             if (!hsync_buf[2] && hsync_old && hcount_delay == 8'hff) begin
                 hsync_converted <= 1;
                 hcount_delay <= 0;
-            end else vsync_converted <= 0;
+            end else hsync_converted <= 0;
         end
     end
     
